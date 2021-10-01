0e7614f00b34392f0c5d6b325c4870a53a4139e9
arielwu415 <ariel070415@gmail.com>
2021-09-30 21:53:38 -0700

    set up TypeScript envirnment

---
+++ components/navbars/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Navbar()
+{
+    return(
+        <header>
+            <nav>
+                <div>hello world</div>
+            </nav>
+        </header>
+    )
+}
