e0acd9a8a27bbc7626102faa8d0f57247592afc1
arielwu415 <ariel070415@gmail.com>
2022-01-21 17:02:13 -0800

    Edited Navbar title and links

--- components/navbars/Navbar.tsx
+++ components/navbars/Navbar.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import Link from 'next/link';
+import styles from '../../styles/components/navbars/Navbar.module.scss'
 
-export default function Navbar()
-{
-    return(
+export default function Navbar() {
+    return (
         <header>
-            <nav>
-                <div>hello world</div>
+            <nav className={styles.nav}>
+                <div className={styles.nav_elements}>
+                    <div className={styles.title}>Ariel Wu.</div>
+                    <div className={styles.right_elements}>
+                        <ul className={styles.nav_links}>
+                            <li>
+                                About
+                            </li>
+                            <li>
+                                Projects
+                            </li>
+                            <li>
+                                GitHub
+                            </li>
+                            <li>
+                                Contact
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </div>
+
             </nav>
         </header>
     )
