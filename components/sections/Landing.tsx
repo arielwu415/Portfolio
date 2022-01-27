ba17d99b05fc986a97da25485b79def88f5fc5d5
arielwu415 <ariel070415@gmail.com>
2022-01-26 22:21:02 -0800

    Edited landing page welcome message

--- components/sections/Landing.tsx
+++ components/sections/Landing.tsx
@@ -5,8 +5,10 @@ export default function Landing() {
     return (
         <section className={styles.section}>
             <div className={styles.main_container}>
-                <h4>Hello;</h4>
-                <span>I'm Ariel Wu. Computer Science Major. Illustrator.</span>
+                <div className={styles.welcome_msg}>
+                    <h2>Hello, I'm Ariel Wu;</h2>
+                    <span> Computer Science Student & Illustrator.</span>
+                </div>
             </div>
         </section>
     )
