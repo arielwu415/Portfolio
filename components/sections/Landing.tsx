70a04778d11d835b08257279a211fa6270ad0f84
arielwu415 <ariel070415@gmail.com>
2022-01-26 20:48:18 -0800

    Edited main container

--- components/sections/Landing.tsx
+++ components/sections/Landing.tsx
@@ -4,7 +4,7 @@ import styles from '../../styles/components/sections/Section.module.scss'
 export default function Landing() {
     return (
         <section className={styles.section}>
-            <div className={styles.welcome_msg}>
+            <div className={styles.main_container}>
                 <h4>Hello;</h4>
                 <span>I'm Ariel Wu. Computer Science Major. Illustrator.</span>
             </div>
