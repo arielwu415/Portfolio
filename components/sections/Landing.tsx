0e4c0c1acac4ebd35ddd239263c89bf4653d8993
arielwu415 <ariel070415@gmail.com>
2022-01-27 22:43:53 -0800

    Added greeting animation

--- components/sections/Landing.tsx
+++ components/sections/Landing.tsx
@@ -1,17 +1,60 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from '../../styles/components/sections/Section.module.scss'
+import { gsap } from "gsap/dist/gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+// gsap.registerPlugin(ScrollTrigger)
 
 export default function Landing() {
+
+    const tl = gsap.timeline({ defaults: { ease: "power1.out" } })
+    let greeting = useRef(null)
+    let close = useRef(null)
+
+    useEffect(() => {
+        tl.fromTo(greeting, {y: 50, opacity: 0}, {duration: 0.7, y: 0, opacity: 1})
+        // gsap.to(close, {
+        //     scrollTrigger: {
+        //         trigger: close,
+        //         start: "top top",
+        //         end: "bottom 100px",
+        //         scrub: 1,
+        //         markers: true
+        //     },
+        //     y: -100
+        // })
+        // gsap.to('.mid', {
+        //     scrollTrigger: {
+        //         trigger: '.mid',
+        //         start: 'top top',
+        //         end: 'bottom 100px',
+        //         scrub: true,
+        //         markers: true
+        //     },
+        //     y: -200
+        // })
+        // gsap.to('.far', {
+        //     scrollTrigger: {
+        //         trigger: '.far',
+        //         start: 'top top',
+        //         end: 'bottom 100px',
+        //         scrub: true,
+        //         markers: true
+        //     },
+        //     y: -300
+        // })
+    }, [])
+
     return (
         <section className={styles.section}>
             <div className={styles.section_container}>
-                <div className={styles.welcome_msg}>
+                <div className={styles.welcome_msg} ref={el => greeting = el}>
                     <h2>Hello, I'm Ariel Wu;</h2>
                     <span> Computer Science Student & Illustrator.</span>
                 </div>
                 <img src="./gray.svg" className={styles.far} alt="" />
                 <img src="./brown.svg" className={styles.mid} alt="" />
-                <img src="./blue.svg" className={styles.close} alt="" />
+                <img src="./blue.svg" className={styles.close} alt="" ref={el => close = el}/>
             </div>
         </section>
     )
