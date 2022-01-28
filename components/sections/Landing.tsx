09a8f52bc508866d5ac7d3274b7a5ec6beb0601e
arielwu415 <ariel070415@gmail.com>
2022-01-27 19:26:30 -0800

    Added landing background codes

--- components/sections/Landing.tsx
+++ components/sections/Landing.tsx
@@ -4,11 +4,14 @@ import styles from '../../styles/components/sections/Section.module.scss'
 export default function Landing() {
     return (
         <section className={styles.section}>
-            <div className={styles.main_container}>
+            <div className={styles.section_container}>
                 <div className={styles.welcome_msg}>
                     <h2>Hello, I'm Ariel Wu;</h2>
                     <span> Computer Science Student & Illustrator.</span>
                 </div>
+                <img src="./gray.svg" className={styles.far} alt="" />
+                <img src="./brown.svg" className={styles.mid} alt="" />
+                <img src="./blue.svg" className={styles.close} alt="" />
             </div>
         </section>
     )
