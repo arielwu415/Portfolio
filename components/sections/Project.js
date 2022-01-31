6223dd871be16f80f438ce03914a73098771d874
arielwu415 <ariel070415@gmail.com>
2022-01-31 15:42:50 -0800

    Created project page component

---
+++ components/sections/Project.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../../styles/components/sections/Project.module.scss'
+
+export default function Project() {
+    return (
+        <section className={styles.section}>
+            <div className={styles.section_container}>
+
+            </div>
+        </section>
+    )
+}
