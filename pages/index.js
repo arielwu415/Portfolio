d127d6c9e3c948d819f8938f4b84d322b16fb504
arielwu415 <ariel070415@gmail.com>
2022-02-01 22:52:30 -0800

    Added async function to grab markdown content

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
 
@@ -5,7 +8,8 @@ import Layout from '../components/layouts/Layout'
 import Landing from '../components/sections/Landing'
 import Project from '../components/sections/Project'
 
-export default function Home() {
+export default function Home({ projects }) {
+
   return (
     <Layout>
       <div className={styles.container}>
@@ -14,10 +18,41 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <Landing/>
-        <Project/>
+        <Landing />
+        <Project posts={projects}/>
+        
 
       </div>
     </Layout>
   )
+}
+
+// Reference: https://www.youtube.com/watch?v=MrjeefD8sac&ab_channel=TraversyMedia
+export async function getStaticProps() {
+  // Get files from the 'markdowns/projects' directory
+  const files = fs.readdirSync(path.join('markdowns/projects'))
+
+  // Get slug and frontmatter from projects
+  const projects = files.map(filename => {
+    const slug = filename.replace('.md', '')
+
+    // Get frontmatter
+    // what's in the .md file
+    const markdownWithMeta = fs.readFileSync(
+      path.join('markdowns/projects', filename),
+      'utf-8'
+    )
+    const { data: frontmatter } = matter(markdownWithMeta)
+
+    return {
+      slug,
+      frontmatter
+    }
+  })
+
+  return {
+    props: {
+      projects
+    }
+  }
 }
