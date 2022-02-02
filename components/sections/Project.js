621bf8a7dcd4d1df4c2f4fbe3769ff06c8102cac
arielwu415 <ariel070415@gmail.com>
2022-02-01 22:56:18 -0800

    Mapped through projects

--- components/sections/Project.js
+++ components/sections/Project.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import styles from '../../styles/components/sections/Project.module.scss'
 
-export default function Project() {
+export default function Project({ posts }) {
+    console.log
     return (
         <section className={styles.section}>
             <div className={styles.section_container}>
-
+                {posts.map((post, index) => (
+                    <h3>{post.frontmatter.title}</h3>
+                ))}
             </div>
         </section>
     )
