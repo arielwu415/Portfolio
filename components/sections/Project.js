fdefff2a8e5b26f92cd8d1877cdc9e4a49dc3371
arielwu415 <ariel070415@gmail.com>
2022-02-03 15:16:06 -0800

    Styled project section

--- components/sections/Project.js
+++ components/sections/Project.js
@@ -6,11 +6,21 @@ export default function Project({ posts }) {
     return (
         <section className={styles.section}>
             <div className={styles.section_container}>
+                <h1 className={styles.section_title}>Projects</h1>
                 <div className={styles.project_container}>
                     {posts.map((post, index) => (
                         <div className={styles.post}>
-                            <h3>{post.frontmatter.title}</h3>
-                            <span>{post.frontmatter.semester}</span>
+                            <div className={styles.img_container}>
+                                <img src={post.frontmatter.cover_image} alt='' />
+                            </div>
+                            <div className={styles.title}>
+                                <h3>{post.frontmatter.title}</h3>
+                                <span>{post.frontmatter.semester}</span>
+                            </div>
+                            <div className={styles.description}>
+                                <span>{post.frontmatter.excerpt}</span>
+                            </div>
+
                         </div>
                     ))}
                 </div>
