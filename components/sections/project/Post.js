5a919fd5d862780d742969c4d4b26042b94fc99a
arielwu415 <ariel070415@gmail.com>
2022-02-03 22:22:27 -0800

    Simplified frontmatter variable

--- components/sections/project/Post.js
+++ components/sections/project/Post.js
@@ -2,18 +2,22 @@ import React from 'react'
 import styles from '../../../styles/components/sections/Project.module.scss'
 
 export default function Post({ post }) {
+
+    const project = post.frontmatter
+
     return (
         <div className={styles.post}>
             <div className={styles.img_container}>
-                <img src={post.frontmatter.cover_image} alt='' />
+                <img src={project.cover_image} alt='' />
             </div>
             <div className={styles.title}>
-                <h3>{post.frontmatter.title}</h3>
-                <span>{post.frontmatter.semester}</span>
+                <h3>{project.title}</h3>
+                <span>{project.semester}</span>
             </div>
             <div className={styles.description}>
-                <span>{post.frontmatter.excerpt}</span>
+                <span>{project.excerpt}</span>
             </div>
         </div>
     )
+    
 }
