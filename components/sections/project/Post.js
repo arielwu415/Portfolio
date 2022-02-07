de542d42ce762896dbb189db0a3d0e79d36520c5
arielwu415 <ariel070415@gmail.com>
2022-02-07 15:38:00 -0800

    Added individual project page

--- components/sections/project/Post.js
+++ components/sections/project/Post.js
@@ -1,23 +1,28 @@
 import React from 'react'
+import Link from 'next/link'
 import styles from '../../../styles/components/sections/Project.module.scss'
 
 export default function Post({ post }) {
 
     const project = post.frontmatter
+    const image = "./project/" + project.cover_image
 
     return (
-        <div className={styles.post}>
-            <div className={styles.img_container}>
-                <img src={project.cover_image} alt='' />
+        <Link href={`/project/${post.slug}`}>
+            <div className={styles.post}>
+                <div className={styles.img_container}>
+                    <img src={image} alt='' />
+                </div>
+                <div className={styles.title}>
+                    <h3>{project.title}</h3>
+                    <span>{project.semester}</span>
+                </div>
+                <div className={styles.description}>
+                    <span>{project.excerpt}</span>
+                </div>
             </div>
-            <div className={styles.title}>
-                <h3>{project.title}</h3>
-                <span>{project.semester}</span>
-            </div>
-            <div className={styles.description}>
-                <span>{project.excerpt}</span>
-            </div>
-        </div>
+        </Link>
+
     )
-    
+
 }
---
+++ pages/project/[slug].js
@@ -0,0 +1,64 @@
+import React from 'react'
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import { marked } from 'marked'
+import Link from 'next/link'
+import styles from '../../styles/Home.module.scss'
+import Layout from '../../components/layouts/Layout'
+
+export default function ProjectPage({
+    frontmatter: { title, semester, cover_image },
+    slug,
+    content
+}) {
+
+    return (
+        <Layout>
+            <div className={styles.container}>
+                <Link href='/'>
+                    <a className={styles.back_button}>Go Back</a>
+                </Link>
+                <div className={styles.content}>
+                    <h1>{title}</h1>
+                    <span>{semester}</span>
+                    <img src={cover_image} alt='' />
+                    <div className={styles.post_body}>
+                        <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+
+    )
+}
+
+export async function getStaticPaths() {
+    const files = fs.readdirSync(path.join('markdowns/projects'))
+
+    const paths = files.map(filename => ({
+        params: {
+            slug: filename.replace('.md', '')
+        }
+    }))
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({ params: { slug } }) {
+    const markdownWithMeta = fs.readFileSync(path.join('markdowns/projects',
+        slug + '.md'), 'utf-8')
+
+    const { data: frontmatter, content } = matter(markdownWithMeta)
+
+    return {
+        props: {
+            frontmatter,
+            slug,
+            content
+        }
+    }
+}
