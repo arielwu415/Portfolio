2df2ec0beea92452eed2bbc0a9b18f05e0653847
arielwu415 <ariel070415@gmail.com>
2022-02-07 12:38:46 -0800

    Created About component

---
+++ components/sections/about/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../../../styles/components/sections/About.module.scss'
+
+export default function About() {
+    return (
+        <section className={styles.section}>
+            <div className={styles.section_container}>
+                <h1 className={styles.section_title}>About</h1>
+                
+            </div>
+        </section>
+    )
+}
