4ad1e227c8d2817d3d7c2a088ca74f409cc42227
arielwu415 <ariel070415@gmail.com>
2022-03-07 22:00:28 -0800

    :star: Added proficiency level percentage

---
+++ components/assets/Level.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from './Level.module.scss'
+
+export const Level = ({ percentage }) => {
+    var width = percentage*100 + "%"
+    console.log(width)
+    return (
+        <div className={styles.level_container}>
+            <div style={{ width: width }} className={styles.upper}>
+                <span>•</span>
+                <span>•</span>
+                <span>•</span>
+                <span>•</span>
+                <span>•</span>
+            </div>
+            <div className={styles.lower}>
+                <span>•</span>
+                <span>•</span>
+                <span>•</span>
+                <span>•</span>
+                <span>•</span>
+            </div>
+        </div>
+    )
+}
