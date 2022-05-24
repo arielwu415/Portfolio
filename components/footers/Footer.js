bfd827f501c3dd44a9407982ec29f2908a750360
arielwu415 <ariel070415@gmail.com>
2022-05-24 13:38:04 -0700

    :zap: Edited footer

--- components/footers/Footer.js
+++ components/footers/Footer.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import styles from '../../styles/components/footers/Footer.module.scss'
 
-export default function Footer () {
-    return (
-        <footer className={styles.footer}>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{' '}
-            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-          </a>
-        </footer>
-    )
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <a
+        href="https://github.com/arielwu415/Portfolio"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Copyrights 2022 / Designed and Developed by Ariel Wu
+        {/*<img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />*/}
+      </a>
+    </footer>
+  )
 }
