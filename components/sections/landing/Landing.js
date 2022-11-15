2c9a045816c4263b915152c8c22f5b481c752690
arielwu415 <ariel070415@gmail.com>
2022-11-15 13:56:50 -0800

    :iphone: Landing blob RWD

--- components/sections/landing/Landing.js
+++ components/sections/landing/Landing.js
@@ -111,8 +111,9 @@ export default function Landing() {
                     className={styles.blob}
                     id='visual'
                     viewBox='0 0 900 550'
-                    width='1080'
+                    width='900'
                     height='720'
+                    overflow='hidden'
                     xmlns='http://www.w3.org/2000/svg'
                     xmlnsXlink='http://www.w3.org/1999/xlink'
                     version='1.1'
